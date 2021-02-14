7fcd098b6bee0c93ead3f1ccdb64c332ed8c63dd
Gromak Maxim <maximgromak@gmail.com>
2021-02-14 21:33:43 +1000

    fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,23 +1,7 @@
 package org.example;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class App {
-
     public static void main(String[] args) throws Throwable {
-        ServerSocket serverSocket = new ServerSocket(25999);
-        int threadsNumber = 64;
-        ExecutorService executorService = Executors.newFixedThreadPool(threadsNumber);
-        while (true) {
-            Socket s = serverSocket.accept();
-            System.err.println("Client accepted");
-
-            executorService.execute(new Server(s));
-        }
+        new Server().start();
     }
-
-
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,127 +1,34 @@
 package org.example;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server implements Runnable {
+public class Server {
 
-    private Socket socket;
-    private InputStream inputStream;
-    private OutputStream outputStream;
+    private int PORT = 29999;
+    private int THREADSNUMBER = 64;
 
-    Server(Socket socket) throws Throwable {
-        this.socket = socket;
-        this.inputStream = socket.getInputStream();
-        this.outputStream = socket.getOutputStream();
-    }
-
-    public void run() {
-        while (true) {
-            try {
-                readInputHeaders();
-                showMainPage();
-            } catch (Throwable t) {
-                /*do nothing*/
-            } finally {
-                try {
-                    socket.close();
-                } catch (Throwable t) {
-                    /*do nothing*/
-                }
+    public void start() throws IOException {
+        Socket clientSocket = null;
+        final ServerSocket serverSocket = new ServerSocket(PORT);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADSNUMBER);
+        try {
+            while (true) {
+                clientSocket = serverSocket.accept();
+                executorService.execute(new HandlerConnection(clientSocket, serverSocket));
             }
+        } finally {
+            stop(clientSocket, serverSocket);
+            System.out.println("Сервер остановлен");
         }
-        //System.err.println("Client processing finished");
+
+    }
+    public void stop(Socket clientSocket, ServerSocket serverSocket) throws IOException {
+        clientSocket.close();
+        serverSocket.close();
     }
 
-    private void writeResponse(String s) throws Throwable {
-        String response = "HTTP/1.1 200 OK\r\n" +
-                "Server: GromakServer/2021-02-13\r\n" +
-                "Content-Type: text/html\r\n" +
-                "Content-Length: " + s.length() + "\r\n" +
-                "Connection: close\r\n\r\n";
-        String result = response + s;
-        outputStream.write(result.getBytes());
-        outputStream.flush();
-    }
-
-    private void readInputHeaders() throws Throwable {
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        while (true) {
-            String s = br.readLine();
-            if (s == null || s.trim().length() == 0) {
-                break;
-            }
-            System.out.println(s);
-            if (s.contains("/page1.html")){
-                showPage1();
-            } else if (s.contains("/page2.html")) {
-                showPage2();
-            } else if(s.contains("/main.html")){
-                showMainPage();
-            }
-
-        }
-        System.out.println("-------------------------------------------------");
-    }
-
-    private void showPage1() throws Throwable {
-        writeResponse("" +
-                "<html>" +
-                "   <body>" +
-                "       <h1>page 1</h1>" +
-                "       <a href=\\page2.html>to go on page 2</a> <br>" +
-                        "<a href=\\main.html>return to main</a>" +
-                "       <style type=\"text/css\">\n" +
-                "           h1 { \n" +
-                "               font-size: 25px; \n" +
-                "               font-family: Verdana, Arial, Helvetica, sans-serif; \n" +
-                "               color: #333366;\n" +
-                "           }\n" +
-                "           a {\n"+
-                "               text-decoration: none;" +
-                "           }\n" +
-                "        </style>"+
-                "   </body>" +
-                "</html>");
-    }
-    private void showPage2() throws Throwable {
-        writeResponse("" +
-                "<html>" +
-                "   <body>" +
-                "       <h1>page 2</h1>" +
-                "       <a href=\\page1.html>to go on page 1</a><br>" +
-                "       <a href=\\main.html>return to main</a>" +
-                "       <style type=\"text/css\">\n" +
-                "           h1 { \n" +
-                "               font-size: 25px; \n" +
-                "               font-family: Verdana, Arial, Helvetica, sans-serif; \n" +
-                "               color: #333366;\n" +
-                "           }\n" +
-                "           a {\n"+
-                "               text-decoration: none;" +
-                "           }\n" +
-                "        </style>"+
-                "   </body>" +
-                "</html>");
-    }
-    private void showMainPage() throws Throwable {
-        writeResponse("" +
-                "<html>" +
-                "   <body>" +
-                "       <h1>Main page</h1>" +
-                "       <a href=\\page1.html>to go on page 1</a><br>" +
-                "       <a href=\\page2.html>to go on page 2</a>" +
-                "       <style type=\"text/css\">\n" +
-                        "           h1 { \n" +
-                        "               font-size: 25px; \n" +
-                        "               font-family: Verdana, Arial, Helvetica, sans-serif; \n" +
-                        "               color: #333366;\n" +
-                        "           }\n" +
-                        "           a {\n"+
-                        "               text-decoration: none;" +
-                        "           }\n" +
-                        "        </style>"+
-                "   </body>" +
-                "</html>");
-    }
 }
